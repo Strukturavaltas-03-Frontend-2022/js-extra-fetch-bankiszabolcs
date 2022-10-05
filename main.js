536e9f6ce8e8f9f38f164d1f18d7dc3135de9b8b
szabi1112 <banki.szabolcs@gmail.com>
2022-10-05 18:12:09 +0200

    kész

---
+++ main.js
@@ -0,0 +1,53 @@
+const url1 = 'https://raw.githubusercontent.com/jokecamp/FootballData/master/UEFA_European_Championship/Euro%202016/players_json/teams.json'
+const url2 = 'https://raw.githubusercontent.com/jokecamp/FootballData/master/UEFA_European_Championship/Euro%202016/players_json/hungary-players.json'
+
+const promise1 = fetch(url1).then(data=>data.json())
+const promise2 = fetch(url2).then(data=>data.json())
+
+const result = await Promise.all([promise1, promise2]).then(data=>data)
+
+const [countries, teams] = result
+
+const team = teams.sheets.Players
+console.log(team);
+const hungary = countries.sheets.Teams.find(element=>element.Team === 'Hungary')
+console.log(hungary);
+
+const infoList = document.querySelector('.infList')
+const playerList = document.querySelector('.playerList')
+
+const makeHungary = ()=>{
+    for(let k in hungary){
+        const li = document.createElement('li')
+        const span = document.createElement('span')
+        span.style.fontWeight = 'bold'
+        span.innerHTML = k;
+        li.appendChild(span)
+        li.innerHTML += `: ${hungary[k]}`
+        infoList.appendChild(li) 
+    }
+}
+
+makeHungary()
+
+const makeTeam = ()=>{
+    team.forEach(element => {
+        const li = document.createElement('li')
+        for(let k in element){
+            if(k==='name'){
+            const span = document.createElement('span')
+            span.style.fontWeight = 'bold'
+            span.innerHTML = element[k];
+            li.appendChild(span)
+            }
+
+            if(k==='date of birth' || k==='position' || k==='club'){
+                li.innerHTML += ` - ${element[k]}`
+                }
+        }
+        playerList.appendChild(li)
+
+    })
+}
+
+makeTeam()
